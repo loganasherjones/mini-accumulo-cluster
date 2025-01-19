992fdf813c0d5fe101a7b4a10c002568b554c697
loganasherjones <loganasherjones@gmail.com>
2025-01-18 21:11:57 -0500

    Temp bump version back for documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 val accumuloVersion = project.property("accumuloVersion").toString()
-version = "${accumuloVersion}-1-SNAPSHOT"
+version = "${accumuloVersion}-0"
 
 mkdocs {
     sourcesDir = "."
